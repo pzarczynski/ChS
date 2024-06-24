e91021a8b1368974dccdb0b274bb8d40fe55dccd
Piotr Żarczyński <piotr.zarczynski.06@gmail.com>
2024-06-24 19:10:16 +0000

    Update code

--- pca.py
+++ pca.py
@@ -1,14 +1,9 @@
 import numpy as np
-import scipy.linalg
 import scipy.sparse
 from tqdm import tqdm
 
 
-def eig(a):
-    return scipy.linalg.eigh(a)
-
-
-def randbool_csc(rand_state, n, m, density):
+def randb_csc(rand_state, n, m, density):
     arr = scipy.sparse.rand(n, m, density, "csc", random_state=rand_state)
     arr.data[:] = 1
     return arr.astype(np.uint8)
@@ -34,9 +29,11 @@ def idxarr_tocsc(arr, cols=None):
     return indices_tocsc(arr + 1, cols + 1)[:, 1:]
 
 
-def jaccard(mat, cols=None):
-    m = cols if cols else (np.max() + 1)
-    mat = idxarr_tocsc(mat, m)
+def bincoef(coef_fn, mat, cols):
+    cols = cols if cols else (np.max() + 1)
+    mat = idxarr_tocsc(mat, cols)
+
+    n, m = mat.shape
     coef = np.empty((m, m), dtype=np.float32)
 
     with tqdm(total=m * (m + 1) // 2) as bar:
@@ -48,11 +45,37 @@ def jaccard(mat, cols=None):
                 v2 = set(mat.indices[s2:r2])
 
                 n11, n01, n10 = len(v1 & v2), len(v1), len(v2)
-                f = n01 + n10 - n11
 
-                coef[i, j] = coef[j, i] = (n11 / f) if f else 0
+                coef[i, j] = coef[j, i] = coef_fn(n, n11, n01, n10)
 
                 j += 1
                 bar.update()
 
     return coef
+
+
+def jaccard(mat, cols=None):
+    def fn(n, n11, n01, n10):
+        d = n01 + n10 - n11
+        return n11 / d if d else 0
+
+    return bincoef(fn, mat, cols)
+
+
+def covariance(mat, cols=None):
+    def fn(n, n11, n01, n10):
+        return (n * n11 - n01 * n10) / (n * (n - 1))
+
+    return bincoef(fn, mat, cols)
+
+
+def eigfix(w, v):
+    v = np.where(w < 0, -v, v)
+    w = np.where(w < 0, -w, w)
+    idx = np.argsort(w)
+    return w[idx], v[idx]
+
+
+def kvectors(eigvals, threshold):
+    cumulative_var = np.cumsum(eigvals[::-1]) / np.sum(eigvals)
+    return np.argmax(cumulative_var > threshold) + 1
